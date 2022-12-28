e3a34a1f337c65ada7ee85b9975860c4a0280068
geninIDTech <117891051+geninIDTech@users.noreply.github.com>
2022-12-28 13:17:30 -0600

    Add files via upload

---
+++ pokemon/original.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include "pokemon.h"
+#include "move.h"
+
+using namespace std;
+
+vector<Pokemon> load_pokemon(const string &filename);
+void load_types(const string &filename, vector<Pokemon> &pokemon);
+vector<Move> load_moves(const string &filename);
+
+
+int main() {
+    return 0;
+}
+
+
+vector<Pokemon> load_pokemon(const string &filename) {
+    return {};
+}
+
+
+void load_types(const string &filename, vector<Pokemon> &pokemon) {
+    return;
+}
+
+
+vector<Move> load_moves(const string &filename) {
+    return {};
+}
